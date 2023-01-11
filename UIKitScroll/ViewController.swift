524b541a83ed3df2dc4909df6322c6432ae89de7
Yury <quasaryy@gmail.com>
2023-01-11 18:48:18 +0400

    Added UIKit Elements

--- UIKitScroll/ViewController.swift
+++ UIKitScroll/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var hugsLabel: UILabel!
+    @IBOutlet var loveLabel: UILabel!
+    @IBOutlet var thanksLabel: UILabel!
+    @IBOutlet var datePicker: UIDatePicker!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var switcher: UISwitch!
+    @IBOutlet var onOffLabel: UILabel!
+    @IBOutlet var button: UIButton!
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var stepper: UIStepper!
+    @IBOutlet var textLabel: UILabel!
+    @IBOutlet var segmentedControll: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func datePickerAction(_ sender: UIDatePicker) {
+    }
+    @IBAction func buttonAction() {
+    }
+    
+    @IBAction func sliderAction(_ sender: UISlider) {
+    }
+    @IBAction func stepperAction(_ sender: UIStepper) {
+    }
+    @IBAction func segmentedControllAction(_ sender: UISegmentedControl) {
+    }
 }
 
